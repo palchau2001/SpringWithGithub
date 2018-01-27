4961a2fed6f8fcbbe7350d347595209c784ec4ea
palchau2001 <palchau2001@yahoo.co.in>
2018-01-27 08:27:27 +0530

    Initial Commit

---
+++ src/main/java/com/spring/github/SpringWithGithubApplication.java
@@ -0,0 +1,12 @@
+package com.spring.github;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWithGithubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWithGithubApplication.class, args);
+	}
+}
---
+++ src/main/java/com/spring/github/controller/TestController.java
@@ -0,0 +1,18 @@
+package com.spring.github.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+//@RequestMapping()
+public class TestController 
+{
+	@RequestMapping(value = {"/", ""}, method = RequestMethod.GET)
+	public ModelAndView indexPage()
+	{		
+		
+		return new ModelAndView("index");
+	}	
+}
